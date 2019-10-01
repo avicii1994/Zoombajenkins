ed4064e6cef0db36deca04076027dbf1c1e17b04
jaiymgit <jai.ramanujapura@ymgit.com>
2019-10-01 11:57:29 +0530

    initial comcommit

---
+++ tests_zoomba.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import datetime
+
+class TestBasicPython(TestCase):
+    def setUp(self):
+        self.share_id = 'abcd1'
+        self.plantDate = datetime.date(2019,1,2)
+        self.price_currency = 2
+        self.variable2 = 2
+        self.variable3 = 8
+
+    def test_id_type(self):
+        pass
+
+    def test_primitive_vals(self):
+        pass
+
+    def test_currency_properties(self):
+        pass
